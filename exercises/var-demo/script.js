26caa437c370acfb9823923b3086f81b7baed109
federico <federicovongromann@gmail.com>
2026-06-01 13:42:01 -0300

    Fix js and add comments

--- exercises/var-demo/script.js
+++ exercises/var-demo/script.js
@@ -1,24 +1,39 @@
-const form = document.getElementById("personForm");
-const validationMessage = document.getElementById("validationMessage");
+const form = document.getElementById('personForm');
+const validationMessage = document.getElementById('validationMessage');
 
-let validationStatus = "pending";
+console.log('form', form);
+console.log('validationMessage', validationMessage);
 
-form.addEventListener("submit", function (event) {
+let validationStatus = 'pending';
+
+form.addEventListener('submit', function (event) {
   event.preventDefault();
 
-  const dni = document.getElementById("dni").value;
-  const name = document.getElementById("name").value;
+  // getElementById obtiene el valor del dni (Buscar una etiqueta html con el id dni)
+  const dni = document.getElementById('dni').value;
+  const name = document.getElementById('name').value;
 
-  const dniIsValid = dni.trim() !== "" && !Number.isNaN(Number(dni));
-  const nameIsValid = name.trim() !== "" && Number.isNaN(Number(name));
+  // La funcion trim se encarga de eliminar los espacios dentro de un string
+  // ' Ana' ->  despues de aplicar trim -> 'Ana'
 
+  // !Number.isNaN(Number(dni))-  Chequea si el valor es un numero y no un string
+  const dniIsValid = dni.trim() !== '' && !Number.isNaN(Number(dni));
+  const nameIsValid = name.trim() !== '' && Number.isNaN(Number(name));
+
+  // Pregunta si dni y name son validos, de ser correcto guarda true
   const formIsValid = dniIsValid && nameIsValid;
 
-  validationStatus = formIsValid ? "valid" : "invalid";
+  // Esto se llama ternaria es parecido a un if, pregunta el valor si es true
+  // en este caso y asinga un valor a validationStatus
+  validationStatus = formIsValid ? 'valid' : 'invalid';
 
-  validationMessage.textContent = Estado: ${validationStatus};
+  // lo que hace es a validationMessage que es una porcion de html asignarle un valor
+  // valid o invalid
+  validationMessage.textContent = `Estado: ${validationStatus}`;
 
-  console.log("DNI:", dni);
-  console.log("Nombre:", name);
-  console.log("Estado:", validationStatus);
+  // Console.log esta diseñada para enviar valores en la dev tools de desarrolador
+  // Para poder imprimir variables
+  console.log('DNI:', dni);
+  console.log('Nombre:', name);
+  console.log('Estado:', validationStatus);
 });
