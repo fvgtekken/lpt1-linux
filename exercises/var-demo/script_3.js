daec3b45f4d5e58892c6226ee5f2939dab186f06
Natalia <nataliav2499@gmail.com>
2026-06-02 14:56:49 -0300

    Add exercise script_3

---
+++ exercises/var-demo/script_3.js
@@ -0,0 +1,22 @@
+let Auto = 1;
+function Set1() {
+
+Auto = 2;
+console.log(Auto);
+}
+
+function Set2() {
+
+  Auto = 3;
+  console.log(Auto);
+}
+
+function Set3() {
+
+  Auto = 4;
+  console.log(Auto);
+}
+
+Set1();
+Set2();
+Set3();
