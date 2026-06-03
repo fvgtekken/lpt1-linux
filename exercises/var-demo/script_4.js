97d6f7fa3acd0a9e1f7f73811b64b1e1b55d3d92
Natalia <nataliav2499@gmail.com>
2026-06-03 13:58:10 -0300

    Add let const exercises

---
+++ exercises/var-demo/script_4.js
@@ -0,0 +1,24 @@
+let Velocidad = 1
+
+function aumentarVelocidad() {
+
+    Velocidad = 'aumentarVelocidad 2';
+    console.log(Velocidad);
+}
+
+function acelerar() {
+
+    Velocidad = 'acelerar 3';
+    console.log(Velocidad);
+}
+
+function velocidadMaxima() {
+
+    let Velocidad = 'velocidadMaxima 4';
+    console.log(Velocidad);
+}
+
+aumentarVelocidad();
+acelerar();
+velocidadMaxima();
+
