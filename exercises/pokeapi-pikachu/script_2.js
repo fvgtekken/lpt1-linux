9d6901905119c11c8bda3205849ea1cd9ec7e6e6
Natalia <nataliav2499@gmail.com>
2026-08-20 19:53:50 -0300

    Add POST for 5 Pokémon

---
+++ exercises/pokeapi-pikachu/script_2.js
@@ -0,0 +1,25 @@
+const pokemonNames = ["pikachu", "bulbasaur", "charmander", "squirtle", "eevee"];
+
+for (let i = 0; i < pokemonNames.length; i++) {
+    const name = pokemonNames[i];
+
+    fetch("https://jsonplaceholder.typicode.com/posts", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ title: name })
+    })
+
+        .then(response => response.json())
+        .then(data => {
+            console.log(data);
+        })
+        .catch(error => console.error(error));
+}
+
+//en terminal:
+//node script_2.js
+//title: 'charmander', id: 101 }
+//{ title: 'squirtle', id: 101 }
+//{ title: 'pikachu', id: 101 }
+//{ title: 'eevee', id: 101 }
+//{ title: 'bulbasaur', id: 101 }
